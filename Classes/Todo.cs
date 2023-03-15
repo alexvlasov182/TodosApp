0acd25874b9ea6b70a514663ed9912ab3f668e7b
Alex Vlasov <drumlife182@gmail.com>
2023-03-15 14:13:07 +0100

    Added css file

---
+++ Classes/Todo.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TodosApp.Classes;
+
+public class Todo
+{
+  public int Id { get; set; }
+  public string? Description { get; set; } = String.Empty;
+  public bool Completed { get; set; } = false;
+}
+
